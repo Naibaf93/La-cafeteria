b30fc5268079127bcf607b19c97900abd164b515
Fabián Huertas <miloh9318@hotmail.com>
2023-09-17 18:24:19 -0500

    añadiendo autoprefixr

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@
 
 const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
 /* Funciones */
 
@@ -11,6 +13,7 @@ function css( done ) {
 
     src('src/scss/app.scss')
         .pipe( sass() )
+        .pipe( postcss([autoprefixer()]) )
         .pipe( dest('build/css'))
 
     done(); 
